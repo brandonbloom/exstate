445b0213cf289a0a83619f6eb0a84b2f69450f15
Brandon Bloom <brandon@brandonbloom.name>
2013-07-01 23:03:50 -0400

    lein new state9

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to state9
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/state9/core_test.clj
@@ -0,0 +1,7 @@
+(ns state9.core-test
+  (:use clojure.test
+        state9.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
